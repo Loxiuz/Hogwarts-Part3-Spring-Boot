2ff2c3e13f7464e16ba3bd532f2f9885db3ccc82
Malte <maltebjoerklund@gmail.com>
2024-04-10 23:53:59 +0200

    Create student with full name

--- src/main/java/edu/hogwarts/dto/StudentDto.java
+++ src/main/java/edu/hogwarts/dto/StudentDto.java
@@ -1,6 +1,8 @@
 package edu.hogwarts.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import edu.hogwarts.model.Student;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +23,7 @@ public class StudentDto {
     private int enrollmentYear;
     private int graduationYear;
     private boolean graduated;
+
     public StudentDto(Student s){
         this.firstName = s.getFirstName();
         this.middleName = s.getMiddleName();
@@ -30,7 +33,28 @@ public class StudentDto {
         this.enrollmentYear = s.getEnrollmentYear();
         this.graduationYear = s.getGraduationYear();
         this.graduated = s.isGraduated();
-
     }
 
+    @JsonCreator
+    public static StudentDto fromFullName(@JsonProperty("name") String fullName){
+        String[] names = fullName.split(" ");
+        StudentDto studentDto = new StudentDto();
+        switch (names.length) {
+            case 3:
+                studentDto.setFirstName(names[0]);
+                studentDto.setMiddleName(names[1]);
+                studentDto.setLastName(names[2]);
+                break;
+            case 2:
+                studentDto.setFirstName(names[0]);
+                studentDto.setLastName(names[1]);
+                break;
+            case 1:
+                studentDto.setFirstName(names[0]);
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid format for full name.");
+        }
+        return studentDto;
+    }
 }
